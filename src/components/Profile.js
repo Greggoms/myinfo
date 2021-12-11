a610ef9e3cae2a48feddd269146328996486fb99
Greg Burton <gpburton@ualr.edu>
2021-12-10 23:38:59 -0600

    build error fix 2

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,28 +6,30 @@ export const Profile = () => {
   const user = netlifyIdentity.currentUser()
   return (
     <>
-      <Seo title={`${user.user_metadata.full_name}'s Profile`} />
+      <Seo
+        title={user ? `${user.user_metadata.full_name}'s Profile` : "Profile"}
+      />
       <div>
-        <h2>{user.user_metadata.full_name}'s Profile</h2>
-        <p>Account Email: {user.email}</p>
-        <p>ID: {user.id}</p>
-        <p>Account Created: {user.created_at.slice(0, 10)}</p>
+        <h2>{user && user.user_metadata.full_name}'s Profile</h2>
+        <p>Account Email: {user && user.email}</p>
+        <p>ID: {user && user.id}</p>
+        <p>Account Created: {user && user.created_at.slice(0, 10)}</p>
         <p>
           Confirmation Email Sent:{" "}
-          {user.confirmation_sent_at
+          {user && user.confirmation_sent_at
             ? user.confirmation_sent_at.slice(0, 10)
             : `Not sent?`}
         </p>
-        <p>Email Confirmed: {user.confirmed_at.slice(0, 10)}</p>
+        <p>Email Confirmed: {user && user.confirmed_at.slice(0, 10)}</p>
         <p>
           Recovery email sent:{" "}
-          {user.recovery_sent_at
+          {user && user.recovery_sent_at
             ? user.recovery_sent_at.slice(0, 10)
             : `No reset link sent.`}
         </p>
         <p>
           Role(s):{" "}
-          {user.app_metadata.roles
+          {user && user.app_metadata.roles
             ? user.app_metadata.roles.map(role => role)
             : "No Role Set"}
         </p>
