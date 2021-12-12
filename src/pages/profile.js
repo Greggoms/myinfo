a827ca8d02cece8e7f77aab369a94ff79a95ce99
Greg Burton <gpburton@ualr.edu>
2021-12-12 15:06:56 -0600

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -87,30 +87,32 @@ const ProfilePage = () => {
             </div>
           </>
         )}
-        <NetlifyIdentityContainer>
-          <h2>Account Info</h2>
-          <p>Email: {user.email}</p>
-          <p>Created: {user.created_at.slice(0, 10)}</p>
-          <p>
-            Confirmation Email Sent:{" "}
-            {user.confirmation_sent_at
-              ? user.confirmation_sent_at.slice(0, 10)
-              : `Not sent? Thats a problem...`}
-          </p>
-          <p>Email Confirmed: {user.confirmed_at.slice(0, 10)}</p>
-          <p>
-            Recovery email sent:{" "}
-            {user.recovery_sent_at
-              ? user.recovery_sent_at.slice(0, 10)
-              : `No reset link sent.`}
-          </p>
-          <p>
-            Role(s):{" "}
-            {user.app_metadata.roles
-              ? user.app_metadata.roles.map(role => role)
-              : "No Role Set"}
-          </p>
-        </NetlifyIdentityContainer>
+        {user && (
+          <NetlifyIdentityContainer>
+            <h2>Account Info</h2>
+            <p>Email: {user.email}</p>
+            <p>Created: {user.created_at.slice(0, 10)}</p>
+            <p>
+              Confirmation Email Sent:{" "}
+              {user.confirmation_sent_at
+                ? user.confirmation_sent_at.slice(0, 10)
+                : `Not sent? Thats a problem...`}
+            </p>
+            <p>Email Confirmed: {user.confirmed_at.slice(0, 10)}</p>
+            <p>
+              Recovery email sent:{" "}
+              {user.recovery_sent_at
+                ? user.recovery_sent_at.slice(0, 10)
+                : `No reset link sent.`}
+            </p>
+            <p>
+              Role(s):{" "}
+              {user.app_metadata.roles
+                ? user.app_metadata.roles.map(role => role)
+                : "No Role Set"}
+            </p>
+          </NetlifyIdentityContainer>
+        )}
       </ProfileContainer>
     </>
   )
