e4ff0d48da6a98fd4de98ba6ec3788dda0db01e2
Greg Burton <gpburton@ualr.edu>
2022-01-05 21:48:26 -0600

    scrollToTop the solution?

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,14 +3,15 @@ import { Router } from "@reach/router"
 import PrivateRoute from "../components/privateRoute"
 import ProfilePage from "./profile"
 import DashboardPage from "./dashboard"
+import ScrollToTop from "../services/scrollToTop"
 
 const App = () => {
   return (
-    <Router primary={false}>
+    <Router>
+      <ScrollToTop path="/" />
       <PrivateRoute path="/app/profile" component={ProfilePage} />
       <PrivateRoute path="/app/dashboard" component={DashboardPage} />
     </Router>
   )
 }
-
 export default App
