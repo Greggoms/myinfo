645f6244158fb9464a26f1b944dd03421c5f10ac
Greg Burton <gpburton@ualr.edu>
2022-07-07 15:43:26 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,8 @@ export const Footer = () => {
       <div className="footer-content">
         Built with
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a> by Greggoms
+        <a href="https://www.gatsbyjs.com">Gatsby</a> by{" "}
+        <a href="https://github.com/Greggoms/myinfo">Greggoms</a>
       </div>
     </FooterContainer>
   )
