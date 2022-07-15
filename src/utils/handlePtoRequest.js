5b30ce99ca35452691a0d4d0f1859f5d87e01440
Greg Burton <gpburton@ualr.edu>
2022-07-15 15:33:10 -0500

    Update handlePtoRequest.js
    Converted it into a try/catch. Requests cannot be approved or denied on mobile. This will tell me why.

--- src/utils/handlePtoRequest.js
+++ src/utils/handlePtoRequest.js
@@ -7,35 +7,40 @@ import { toast } from "react-toastify"
 const handlePtoRequest = (e, id, index) => {
   const person = store.getState().users.value.find(person => person.id === id)
   const userRef = doc(db, "users", person.id)
-  if (e.target.id === "approve") {
-    store.dispatch(
-      approvePtoRequest({
-        id: person.id,
-        request: person.pto.submitted[index],
-      })
-    )
-    async function approvePto() {
-      await updateDoc(userRef, {
-        "pto.submitted": arrayRemove(person.pto.submitted[index]),
-        "pto.pending": arrayUnion(person.pto.submitted[index]),
-      })
+  try {
+    if (e.target.id === "approve") {
+      store.dispatch(
+        approvePtoRequest({
+          id: person.id,
+          request: person.pto.submitted[index],
+        })
+      )
+      async function approvePto() {
+        await updateDoc(userRef, {
+          "pto.submitted": arrayRemove(person.pto.submitted[index]),
+          "pto.pending": arrayUnion(person.pto.submitted[index]),
+        })
+      }
+      approvePto()
+      toast.success(
+        `${person.name}'s request for ${person.pto.submitted[index].dates} has been approved!`
+      )
+    } else {
+      store.dispatch(
+        denyPtoRequest({ id: person.id, request: person.pto.submitted[index] })
+      )
+      async function denyPto() {
+        await updateDoc(userRef, {
+          "pto.submitted": arrayRemove(person.pto.submitted[index]),
+          "pto.denied": arrayUnion(person.pto.submitted[index]),
+        })
+      }
+      denyPto()
+      toast.info(`${person.name}'s request has been denied.`)
     }
-    approvePto()
-    toast.success(
-      `${person.name}'s request for ${person.pto.submitted[index].dates} has been approved!`
-    )
-  } else {
-    store.dispatch(
-      denyPtoRequest({ id: person.id, request: person.pto.submitted[index] })
-    )
-    async function denyPto() {
-      await updateDoc(userRef, {
-        "pto.submitted": arrayRemove(person.pto.submitted[index]),
-        "pto.denied": arrayUnion(person.pto.submitted[index]),
-      })
-    }
-    denyPto()
-    toast.info(`${person.name}'s request has been denied.`)
+  } catch (e) {
+    console.error(e.message)
+    toast.error(e.message)
   }
 }
 
